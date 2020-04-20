a438c34fd314d356c1149154c78c9d23324de9da
Nico D'Cotta <nico.dc@outlook.com>
2020-04-20 16:02:15 +0200

    Write tested dists for mock algorithms exam

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,23 @@
 module Main where
 
+main :: IO ()
+main = putStr $ show (dists "hello" "bhell")
 
-letters xs = [take n xs | n <- [0..length xs]]
+letters xs = [take n xs | n <- [0 .. length xs]]
 
+dists :: String -> String -> [String]
 dists xs [] = reverse $ letters xs
-dists [] = letters
-dists X@(x:xs) Y@(y:ys) =
-  minimumBy length  [X : dists xs Y,
-                     (dists X ys) ++ [Y]
-                     X' ++ (dists xs ys)]
-    where X' = if x==y [] else [X]
-
+dists [] xs = letters xs
+dists x'@(x:xs) y'@(y:ys) = minimumBy length [x' : dists xs y', dists x' ys ++ [y'], ds']
+  where
+    ds = dists xs ys
+    ds' =
+      if x == y
+        then map (x :) ds
+        else x' : map (y :) ds
+    minimumBy :: (a -> Int) -> [a] -> a
+    minimumBy _ [x] = x
+    minimumBy f (x:y:xs) =
+      if f x > f y
+        then minimumBy f (y : xs)
+        else minimumBy f (x : xs)
